d36df78d234d3ca5fdf71209dbf420b0268c360c
Elophe <olivier.elophe@gmail.com>
2019-06-18 18:59:19 +0200

    Fichiers pour l'énnoncé du TP

---
+++ NSI_TP_JS/js/Favoris.js
@@ -0,0 +1,28 @@
+/*
+TP - Introduction à a Javascript pour le Web
+*/
+// Liste des liens Youtube à afficher. Un lien est défini par :
+// - son titre
+// - son URL
+// - Le proriétaire de la chaine
+var listeLiens = [
+    {
+        titre: "Science étonnante",
+        url: "https://www.youtube.com/scienceetonnante",
+        auteur: "David Louapre"
+    },
+    {
+        titre: "Grain de philo",
+        url: "https://www.youtube.com/channel/UCqA8H22FwgBVcF3GJpp0MQw",
+        auteur: " Thibaut Giraud"
+    },
+    {
+        titre: "Grafikart.fr",
+        url: "https://www.youtube.com/channel/UCj_iGliGCkLcHSZ8eqVNPDQ",
+        auteur: "Grafikart"
+    }
+];
+
+//***********************************
+// Complétez le code à partir d'ici :
+//***********************************
